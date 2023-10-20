1d18ae5cb29cdeee85ee02f69abc71626ce3e26e
Naveen Kumar <nk24528.nk81@gmail.com>
2023-10-20 15:27:40 +0530

    one prime problem solution

---
+++ prime.js
@@ -0,0 +1,19 @@
+let num = 13;
+
+let count = 0;
+for(let i = 1; i <= num; i++)
+{
+    if(num % i == 0)
+    {
+        count++
+    }
+}
+
+if(count != 2)
+{
+    console.log("Is Prime");
+}
+else
+{
+    console.log("Isn't Prime");
+}
