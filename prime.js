ed20cfd89272f80a15528804ad419605354221a9
Vinay Kumar <vinaysikhera2203@gmail.com>
2023-10-20 15:42:33 +0530

    i fixed the problem with the help of function

--- prime.js
+++ prime.js
@@ -1,19 +1,23 @@
-let num = 13;
 
-let count = 0;
-for(let i = 1; i <= num; i++)
-{
-    if(num % i == 0)
+
+
+function checkPrime(num){
+    let count = 0;
+    for(let i = 1; i <= num; i++)
     {
-        count++
+        if(num % i == 0)
+        {
+            count++
+        }
+    }
+    
+    if(count != 2)
+    {
+        console.log("Is Prime");
+    }
+    else
+    {
+        console.log("Isn't Prime");
     }
 }
-
-if(count != 2)
-{
-    console.log("Is Prime");
-}
-else
-{
-    console.log("Isn't Prime");
-}
+checkPrime(13)
